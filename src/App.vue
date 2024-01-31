60d151cc8277afff06711be030b5d11d8ff4b00b
DavidTobia89 <davidtobiadelbusto@gmail.com>
2024-01-31 11:04:34 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script setup>
+import Formulario from './components/Formulario.vue'
+import Clima from './components/Clima.vue'
+import Spinner from './components/Spinner.vue'
+import useClima from './composables/useClima'
+import Alerta from './components/Alerta.vue'
+const {obtenerClima, clima, mostrarClima, cargando, error} = useClima()
+
+</script>
+
+<template>
+  <h1 class="titulo">Buscador de Clima</h1>
+  <div class="contenedor buscador-clima">
+    <Formulario
+    @obtener-clima="obtenerClima"
+    ></Formulario>
+    <Alerta v-if="error">{{ error }}</Alerta>
+    <Spinner v-if="cargando"></Spinner>
+    <Clima v-if="mostrarClima"
+    :clima="clima"
+    ></Clima>
+    
+  </div>
+    
+</template>
+
+<style scoped>
+
+</style>
